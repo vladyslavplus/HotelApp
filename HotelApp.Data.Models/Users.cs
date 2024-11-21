15eba8cb3d9973f217a3d91f404e76db32cb8dc2
= <=>
2024-11-21 14:56:18 +0200

    Add project files.

---
+++ HotelApp.Data.Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Data.Models
+{
+    public class Users : IdentityUser
+    {
+        
+    }
+}
---
+++ HotelApp/Data/HotelDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HotelApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelApp.Data
+{
+    public class HotelDbContext : IdentityDbContext<Users>
+    {
+        public HotelDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
